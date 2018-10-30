a0f7b3b9d6247494fe12a7eeda380ad1ecdc9b1a
Igor Morais <igormorais@MacBook-Pro-de-Igor.local>
2018-10-30 14:36:48 -0300

    first commit

---
+++ src/main/java/com/teste/campfire/CampfireApplication.java
@@ -0,0 +1,12 @@
+package com.teste.campfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampfireApplication.class, args);
+	}
+}
